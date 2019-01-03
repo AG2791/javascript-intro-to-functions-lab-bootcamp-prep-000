771646799d3a1728cc75d3391485c500bcaf955e
AJ Guiles <ashiguiles@gmail.com>
2019-01-03 06:23:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ logShout('hello')
 
 logWhisper('hello')
 
-var lowvolume= "I Can't hear"
+var lowVolume = "I can't hear you"
+var highVoume = 
 function sayHiToGrandma(string) {
   if string = toLowerCase() 
    return
