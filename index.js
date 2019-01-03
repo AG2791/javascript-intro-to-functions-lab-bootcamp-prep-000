8ea767247ea57727f8b03da7d9147fda68026ac1
AJ Guiles <ashiguiles@gmail.com>
2019-01-03 06:31:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ logShout('hello')
 
 logWhisper('hello')
 
-var lowVolume = "I can't hear you"
-var highVoume = "YES Indeed"
-var reponse = "I love you, too"
+var lowVolume = "I can't hear you";
+var highVoume = "YES Indeed";
+var reponse = "I love you, too";
 
-function sayHiToGrandma(string) {
+function sayHiToGrandma(string){
   if string = toLowerCase() 
    return lowVolume
    
