318a9115eac3001f1316c544081d65c88bff946d
AJ Guiles <ashiguiles@gmail.com>
2019-01-03 06:45:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ function sayHiToGrandma(string){
    else if (string === toUpperCase())
    return "YES Indeed" ;
    
-   else if (string === "I Love you, grandma";
+   else if (string === "I Love you, grandma");
+   return "I love you, too"
 }
 
   
